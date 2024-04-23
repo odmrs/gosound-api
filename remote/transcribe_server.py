f071d41eba014273840038909f80bfadba3350d2
Marcos Vinicius <omarcosviniciusdev@gmail.com>
2024-04-23 20:48:20 -0300

    feat: add api to transcribe a audio to text using python

---
+++ remote/transcribe_server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from vosk import Model, KaldiRecognizer
+from pydub import AudioSegment
+import json
+
+app = Flask(__name__)
+model = Model("./models")
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    if 'file' not in request.files:
+        return "No file part", 400
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file", 400
+
+    try:
+        sound = AudioSegment.from_file(file, format="mp3")
+
+        rec = KaldiRecognizer(model, sound.frame_rate) 
+
+        for chunk in sound[::1000]:  
+            rec.AcceptWaveform(chunk.raw_data)  
+
+        result = json.loads(rec.FinalResult())
+        return jsonify(result)
+    except Exception as e:
+        return str(e), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
